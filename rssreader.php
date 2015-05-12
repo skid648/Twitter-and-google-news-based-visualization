1e445c303c91418d523bf9aa1a52cc3ac83941bf
skid648 <thagbmx@hotmail.com>
2015-05-12 04:23:25 +0300

    update

--- rssreader.php
+++ rssreader.php
@@ -119,7 +119,7 @@
 				    
 				    //$link = GetImage($key);
 				    //echo "<div style='overflow:hidden; text-align:center; width:220px; height:220px; float:left; border-radius:250px;'><img style='width:auto; height:200px;' src=".$link."></img>".$key."</div>";
-				    $sql = "INSERT INTO Hot (Name,image_url ) VALUES ('".$key."','".GetImage($key)."')";
+				    $sql = "INSERT INTO Hot (Name,image_url ) VALUES ('".$key."','".GetImage($key,$conn)."')";
 
 					if ($conn->query($sql) === TRUE) {
 					    //echo "done";
@@ -142,14 +142,27 @@
 		        return str_replace($search, $replace, $string); 
 		} 
 
-		function GetImage($name){
+		function GetImage($name,$conn){
+
+			$sql = "SELECT FirstName, LastName FROM vouleutes WHERE LastName = '".$name."'";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+			    // output data of each row
+			    while($row = $result->fetch_assoc()) {
+			        //echo "Onoma: " . $row["FirstName"]. " - Epitheto: " . $row["LastName"]. "<br>";
+			        $name =  $row["FirstName"]."%20".$row["LastName"];
+			    }
+			} else {
+			    echo "0 results";
+			}
 
 			$google_search_api = "https://www.googleapis.com/customsearch/v1";
 			$key = "AIzaSyDrIcRT3ETSei8Rhh09StVO2dUwrudWPn0";
 			$id="010381646192131644412:zp1d2xj-tmk";
 			$google_search_api = $google_search_api."?key=".$key;
 			$google_search_api = $google_search_api."&cx=".$id;
-			$google_search_api = $google_search_api."&q=".$name."%20Πολιτικός";
+			$google_search_api = $google_search_api."&q=".$name;
 			$google_search_api = $google_search_api.'&searchType=image';
 			$body = file_get_contents($google_search_api);
 			$json = json_decode($body);
