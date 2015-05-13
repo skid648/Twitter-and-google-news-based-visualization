f02f8f4f7d972f29974eec22fea307c9badd63e1
skid648 <thagbmx@hotmail.com>
2015-05-13 19:30:34 +0300

    update

--- backend/find_urls.php
+++ backend/find_urls.php
@@ -10,55 +10,39 @@ require("credentials.php");
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $deputies = Array();
-$sql = "SELECT FirstName, LastName FROM vouleutes ";
+$sql = "SELECT FirstName, LastName ,image FROM vouleutes ";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) {
 			    // output data of each row
 			    while($row = $result->fetch_assoc()) {
 			        //echo "Onoma: " . $row["FirstName"]. " - Epitheto: " . $row["LastName"]. "<br>";
-			        $name =  Array("name"=>$row["FirstName"],"last"=> $row["LastName"],"query"=>$row["FirstName"]."%20".$row["LastName"]);
+			        $name =  Array("name"=>$row["FirstName"],"last"=> $row["LastName"],"image"=>$row["image"]);
 			        array_push($deputies, $name);
 			    }
 			} else {
 			    echo "0 results";
 			}
 
-
+				echo "<div style='background-color:#799DA2;'>";
 			foreach ($deputies as $key => $value) {
-				echo $value["name"]." ".$value["last"]." url:  ".$value["query"]."<br>";
-				$sql = "UPDATE vouleutes SET image='".GetImage($value["query"])."' WHERE FirstName='".$value["name"]."' AND LastName='".$value["last"]."'";
-				echo $sql."<br>";
-				if ($conn->query($sql) === TRUE) {
-				    echo "Record updated successfully";
-				} else {
-				    echo "Error updating record: " . $conn->error;
-				}
+				//echo $value["name"]." ".$value["last"]." url:  ".$value["image"]."<br>";
+				echo "<form style='background-color:#799DA2; margin:5px;'action='updatedb.php' method='post'>";
+				echo "<div style='margin: 10px; padding-bottom:150px; background-color:#BFF9FF; border:1px solid dashed'>";
+				echo "<img src=".$value['image']." style =' margin:5px; width:50px;height:50px;float:left;'></img>";
+				echo "<div style=' margin:5px; height:80px; width:100%;'><div style=' margin:5px; overflow:hidden;'>".$value["name"]." ".$value["last"]."<br>url: ".$value["image"]."</div><br>";
+				echo "<input style='width:80%;'></input><button style='width:9%; background-color:black; color:white;' type='submit'>Submit</button></div>";
+				echo "</div>";
+				echo "</form>";
 			}
-
+				echo "</div>";
 
 
 			
 
 
 
-function GetImage($name){
 
-			
-
-			$google_search_api = "https://www.googleapis.com/customsearch/v1";
-			$key = "AIzaSyDrIcRT3ETSei8Rhh09StVO2dUwrudWPn0";
-			$id="010381646192131644412:zp1d2xj-tmk";
-			$google_search_api = $google_search_api."?key=".$key;
-			$google_search_api = $google_search_api."&cx=".$id;
-			$google_search_api = $google_search_api."&q=".$name;
-			$google_search_api = $google_search_api.'&searchType=image';
-			echo "</br>".$google_search_api."</br>";
-			$body = file_get_contents($google_search_api);
-			$json = json_decode($body);
-			$link = $json->items[1]->link;
-			return $link;
-		}
 ?>
 </body>
 </html>
---
+++ backend/updatedb.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
+<?php
+
+require("credentials.php");
+
+//$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+echo "<br>alright<br>";
+
+?>
+</body>
+</html>
